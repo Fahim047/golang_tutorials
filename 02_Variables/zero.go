bd2954e6c16b40ebe3b9aaab059ec4493bc37ec6
Fahim047 <fahimulislam58@gmail.com>
2021-02-11 08:42:31 +0600

    Zero Values of Variables

---
+++ 02_Variables/zero.go
@@ -0,0 +1,19 @@
+// zero values or  default values of variables
+package main
+
+import "fmt"
+
+func main() {
+
+	var (
+		a int
+		b string
+		c float64
+		d bool
+	)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	
+}
