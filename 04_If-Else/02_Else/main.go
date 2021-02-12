c100ddbbb48e61081d874e1f02e0f4d5b4b0ee98
Fahim047 <fahimulislam58@gmail.com>
2021-02-13 00:08:37 +0600

    If-else

---
+++ 04_If-Else/02_Else/main.go
@@ -0,0 +1,21 @@
+// if-else statement 
+package main
+
+import "fmt"
+
+func main() {
+
+	x := 47
+	if x>=47 {
+		fmt.Println("aye aye: ",x)
+	} else {
+		fmt.Println("no no no: ",x)
+	}
+
+	a := 7
+	if a%2==0 {
+		fmt.Println("Even")
+	} else {
+		fmt.Println("Odd")
+	}
+}
