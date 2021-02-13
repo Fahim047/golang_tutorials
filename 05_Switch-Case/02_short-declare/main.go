4a8cb6d41cabe7cc54edb0a708e6de280866a1e4
Fahim047 <fahimulislam58@gmail.com>
2021-02-13 09:01:11 +0600

    short declaration

---
+++ 05_Switch-Case/02_short-declare/main.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+
+func main() {
+	switch color :="pink"; color {
+		case "black":
+			fmt.Println("Black")
+		case "blue": 
+			fmt.Println("Blue")
+		case "pink": 
+			fmt.Println("Pink!!!!!!!!")
+		case "orange": 
+			fmt.Println("Orange")
+		case "yellow": 
+			fmt.Println("Yellow")
+		case "red": {
+			fmt.Println("RED")
+			fmt.Println("opssss!!! color of blood")
+		}
+		default: fmt.Println("color didn't match")
+	}
+}
