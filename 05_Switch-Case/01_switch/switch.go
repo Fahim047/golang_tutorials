259451905f240a62db9bcb2d504a9ff2bf315965
Fahim047 <fahimulislam58@gmail.com>
2021-02-13 08:58:24 +0600

    basic switch statement

--- 05_Switch-Case/01_switch/switch.go
+++ 05_Switch-Case/01_switch/switch.go
@@ -5,15 +5,15 @@ func main() {
 	var color = "red"
 	switch color {
 		case "black":
-			fmt.Println("Monday")
+			fmt.Println("Black")
 		case "blue": 
-			fmt.Println("Tuesday")
+			fmt.Println("Blue")
 		case "pink": 
-			fmt.Println("Wednesday")
+			fmt.Println("Pink")
 		case "orange": 
-			fmt.Println("Thursday")
+			fmt.Println("Orange")
 		case "yellow": 
-			fmt.Println("Friday")
+			fmt.Println("Yellow")
 		case "red": {
 			fmt.Println("RED")
 			fmt.Println("opssss!!! color of blood")
