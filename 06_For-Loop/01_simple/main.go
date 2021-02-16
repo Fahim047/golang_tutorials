529102afcb8600436700490796fa29b5ca24b474
Fahim047 <fahimulislam58@gmail.com>
2021-02-16 09:27:13 +0600

    simple for loop

---
+++ 06_For-Loop/01_simple/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	//syntax
+	/*for initialization; condition; increment-decrement{
+		loop body
+	}
+	*/
+
+	for i:=0; i<10; i++ {
+		fmt.Println(i)
+	}
+}
