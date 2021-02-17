61f2070a2c1d5f7f3f82f2b6374c23f77f3002af
Fahim047 <fahimulislam58@gmail.com>
2021-02-17 08:35:56 +0600

    infinite loop

---
+++ 06_For-Loop/05_infinite-loop/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	for { 			//infinite loop
+		fmt.Println("Allah is Almighty") 
+	}
+
+	//close the program as soon as possible.
+	//it may crash your system.
+}
